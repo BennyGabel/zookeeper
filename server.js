c927af1a6f9e14cc2c85252c1aa515599ee8803b
13 Miles <bennygabel@gmail.com>
2022-10-06 15:04:12 -0400

    update changes as of 22.010.06 @15:04

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const app = express()
 
 app.listen(3001, () => {console.log('API servr now on port 3001!')});
 
-app.get('/api/animals', (req, res) => {
-    // res.send('Hello!'); 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+  
 
-    let result = animals;
-    console.log(req.query)
-    res.json(animals);
-})
-
+  app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+  
 const{ animals } = require('./data/animals');
