c0fa51c0d352a5348e899ba9a3a7f4d408680918
13 Miles <bennygabel@gmail.com>
2022-10-26 23:29:25 -0400

    front-end end of lesson 11.3

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 }); 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 /* 22.10.26 */
 
 
